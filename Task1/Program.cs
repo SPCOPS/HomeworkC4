8f29332a860b4d02092819dadc0f4248b00423e0
SPECTR <ivanov8465@mail.ru>
2023-03-22 19:22:08 +0300

    Сделал 1 задание

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿int step (int a, int b)
+{
+  int result = 1;
+  for (int i = 1; i <= b; i++)
+  {
+    result = result * a;
+  }
+return result;
+}
+Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int x = step (a, b);
+Console.WriteLine($"Ответ: {x}");
