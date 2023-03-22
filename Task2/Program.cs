a28a487572a5ced304d023089b7d427817d4e8e2
SPECTR <ivanov8465@mail.ru>
2023-03-22 19:22:21 +0300

    Сделал 2 задание

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int sumnumb (int a)
+{
+  int result = 0;
+  while (a > 0)
+  {
+    result += a % 10;
+    a = a / 10;
+  }
+return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int x = sumnumb (a);
+Console.WriteLine($"Ответ: {x}");
